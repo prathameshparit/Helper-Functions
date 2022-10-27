7afc5ad82b7f75fa18eca6fc1bfa84815dd4e0fa
Prathamesh Parit <63944541+prathameshparit@users.noreply.github.com>
2022-10-27 19:26:37 +0530

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -89,10 +89,6 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
   ax.xaxis.set_label_position("bottom")
   ax.xaxis.tick_bottom()
 
-  ### Added: Rotate xticks for readability & increase font size (required due to such a large confusion matrix)
-  plt.xticks(rotation=70, fontsize=text_size)
-  plt.yticks(fontsize=text_size)
-
   # Set the threshold for different colors
   threshold = (cm.max() + cm.min()) / 2.
 
@@ -290,50 +286,3 @@ def calculate_results(y_true, y_pred):
                   "recall": model_recall,
                   "f1": model_f1}
   return model_results
-
-
-
-# Before and after plot_loss_curves
-
-def compare_historys(original_history, new_history, initial_epochs=5):
-    """
-    Compares two model history objects.
-    """
-    # Get original history measurements
-    acc = original_history.history["accuracy"]
-    loss = original_history.history["loss"]
-
-    print(len(acc))
-
-    val_acc = original_history.history["val_accuracy"]
-    val_loss = original_history.history["val_loss"]
-
-    # Combine original history with new history
-    total_acc = acc + new_history.history["accuracy"]
-    total_loss = loss + new_history.history["loss"]
-
-    total_val_acc = val_acc + new_history.history["val_accuracy"]
-    total_val_loss = val_loss + new_history.history["val_loss"]
-
-    print(len(total_acc))
-    print(total_acc)
-
-    # Make plots
-    plt.figure(figsize=(8, 8))
-    plt.subplot(2, 1, 1)
-    plt.plot(total_acc, label='Training Accuracy')
-    plt.plot(total_val_acc, label='Validation Accuracy')
-    plt.plot([initial_epochs-1, initial_epochs-1],
-              plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
-    plt.legend(loc='lower right')
-    plt.title('Training and Validation Accuracy')
-
-    plt.subplot(2, 1, 2)
-    plt.plot(total_loss, label='Training Loss')
-    plt.plot(total_val_loss, label='Validation Loss')
-    plt.plot([initial_epochs-1, initial_epochs-1],
-              plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
-    plt.legend(loc='upper right')
-    plt.title('Training and Validation Loss')
-    plt.xlabel('epoch')
-    plt.show()
